6043e865bdee7e381561fc8fefe4ca1f16687950
Hsiu-Fan Wang <hfwang@porkbuns.net>
2016-02-21 22:34:07 -0800

    Initial commit

---
+++ lib/omniauth/wink/password.rb
@@ -0,0 +1,9 @@
+require "omniauth/wink/password/version"
+
+module Omniauth
+  module Wink
+    module Password
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/omniauth/wink/password/version.rb
@@ -0,0 +1,7 @@
+module Omniauth
+  module Wink
+    module Password
+      VERSION = "0.1.0"
+    end
+  end
+end
